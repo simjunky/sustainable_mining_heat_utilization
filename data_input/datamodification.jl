23e486b80427dee961e5a45a901393faf52a3fd0
simjunky <patrick.martin.2@outlook.de>
2021-08-18 09:31:13 +0200

    add scenario column to input data

--- data_input/datamodification.jl
+++ data_input/datamodification.jl
@@ -14,19 +14,31 @@ plot_file = "plots/heat_demand.pdf"
 
 
 # read from .csv-file and store only the columns containing hour and load information into a DataFrame
-data = DataFrame(CSV.File(origin_file, delim=',', select=[:hour, :load]))
+load_data = DataFrame(CSV.File(origin_file, delim=',', select=[:hour, :load]))
 
 
 #TODO: load is unitless and normalized to 1 000 000 => scale to the average household so that it is given in [Kwh]
 
 
 # use the @df macro to easily plot the heat load over the year and store it as a plot
-demand_plot = @df data plot(:hour, :load, title = "heat demand for NUTS-DE11: Stuttgart", xlabel = "Hours of the year [h]", ylabel = "heat load []")
+demand_plot = @df load_data plot(:hour, :load, title = "heat demand for NUTS-DE11: Stuttgart", xlabel = "Hours of the year [h]", ylabel = "heat load []")
 
 
 # write the stored plot to a .pdf-file
 savefig(demand_plot, plot_file)
 
 
-# write the reduced and scaled data withput headers to the target destination .csv-file to use as model input
-CSV.write(target_file, data, writeheader = false)
+# get the number of rows in the load data
+n = nrow(load_data)
+
+
+# create a column containing the scenario name
+scenario_data = DataFrame( scenario = "Germany_Stuttgart", hour = 1:n)
+
+
+# join the scenario column to the load data DataFrame from the left
+input_data = innerjoin(scenario_data, load_data, on = :hour)
+
+
+# write the reduced and scaled data without headers to the target destination .csv-file to be used as model input
+CSV.write(target_file, input_data, writeheader = false)
