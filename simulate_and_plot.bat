ec765aba2afec89b3c800daaa6a0977d7a697898
simjunky <patrick.martin.2@outlook.de>
2021-10-22 16:33:38 +0200

    adapt batch file to scenario folder structure

--- simulate_and_plot.bat
+++ simulate_and_plot.bat
@@ -5,17 +5,19 @@ echo command line arguments are used as scenarios
 set list=%*
 ) else (
 echo default arguments are used
-set list=Germany_Stuttgart Finnland_Helsinki Canada_Ottawa
+set list=Germany_Stuttgart Argentina_RioGrande Sudan_Karthoum
 )
+echo STARTING COMPUTATION
 echo the following scenarios will be computed:
 echo %list%
 for %%i in (%list%) do (
-if not exist %%i (
+if not exist scenarios\%%i (
 echo no scenario folder found for %%i
 ) else (
-if not exist %%i\data_output mkdir %%i\data_output
-if not exist %%i\plots mkdir %%i\plots
+if not exist scenarios\%%i\data_output mkdir scenarios\%%i\data_output
+if not exist scenarios\%%i\plots mkdir scenarios\%%i\plots
 echo COMPUTING SCENARIO %%i
+echo here should be invocation of gams and plotting
 )
 )
-echo computations complete
+echo ALL COMPUTATIONS COMPLETE
